296dc95d872782c34bed0255abfb00240a74eb41
Rania Yqb <raniayaqoubi1995@gmail.com>
2023-02-03 15:27:57 +0100

    Adding first React Components

---
+++ assets/react/controllers/HomeForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomeForm () {
+    return <div>Hello </div>;
+}
+
+export default HomeForm;
--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
